eac40915802959135bc6bba7f978696550fc397b
Lukas Larsed <git@lukaslarsed.com>
2015-01-11 14:47:54 +0100

    Started on class version

---
+++ f1/RouletteWithClasses/src/Roulette.cpp
@@ -0,0 +1,18 @@
+/*
+ * Roulette.cpp
+ *
+ *  Created on: Jan 11, 2015
+ *      Author: luke
+ */
+
+#include "Roulette.h"
+
+Roulette::Roulette() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Roulette::~Roulette() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ f1/RouletteWithClasses/src/Roulette.h
@@ -0,0 +1,23 @@
+/*
+ * Roulette.h
+ *
+ *  Created on: Jan 11, 2015
+ *      Author: luke
+ */
+
+#ifndef ROULETTE_H_
+#define ROULETTE_H_
+
+class Roulette {
+
+private:
+	int spin_count;
+	int const MIN = 0, MAX = 1;
+	int seq
+
+public:
+	Roulette();
+	virtual ~Roulette();
+};
+
+#endif /* ROULETTE_H_ */
